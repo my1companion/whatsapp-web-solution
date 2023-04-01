7a6c2b2cbf9a073a29616637ddd857b0201b1d4d
Adeyinka Adebowale <adeyinkaadebowale.bincom@gmail.com>
2023-04-01 20:04:04 +0100

    testing

--- example.js
+++ example.js
@@ -1,13 +1,34 @@
-const { Client, Location, List, Buttons, LocalAuth} = require('./index');
+const { Client, Location, List, Buttons, LocalAuth, RemoteAuth} = require('./index');
 const http = require('http');
 
 
-const client = new Client({
-    authStrategy: new LocalAuth(),
-    puppeteer: { headless: true}
-});
+// const client = new Client({
+//     authStrategy: new LocalAuth(),
+//     puppeteer: { headless: true}
+// });
 
-client.initialize();
+// client.initialize();
+
+// const { Client, RemoteAuth } = require('whatsapp-web.js');
+
+// Require database
+const { MongoStore } = require('wwebjs-mongo');
+const mongoose = require('mongoose');
+
+
+// Load the session data
+mongoose.connect(process.env.MONGODB_URI).then(() => {
+    const store = new MongoStore({ mongoose: mongoose });
+    const client = new Client({
+        authStrategy: new RemoteAuth({
+            store: store,
+            backupSyncIntervalMs: 300000
+        })
+    });
+
+    client.initialize();
+});
+ 
 
 client.on('loading_screen', (percent, message) => {
     console.log('LOADING SCREEN', percent, message);
